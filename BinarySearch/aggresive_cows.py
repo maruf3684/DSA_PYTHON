3a83b02cb150a51b70082d52eb7d1c4c439d7295
maruf <maruf.hasan@tallykhata.com>
2025-09-16 13:57:01 +0600

    change in main file root level

---
+++ BinarySearch/aggresive_cows.py
@@ -0,0 +1,25 @@
+from BinarySearch.first_and_last_position import right
+
+cows = [1, 2, 4, 8, 9]  #0,,2,,3   //3
+k = 3
+
+cows = [10, 1, 2, 7, 5] #0,1,4    //4
+#sort = [1,2,5,7,10]   #0, 2 ,4
+k = 3
+
+
+def find_max_distance(cows,k):
+    cows = sorted(cows)
+    left = 0
+    right  = cows[-1] - cows[0]
+
+
+
+
+def distance(cows,k,mid):
+    count = 0
+
+
+
+
+#print(find_max_distance(cows,k))
