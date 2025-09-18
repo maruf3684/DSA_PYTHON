6a3eecf56fdae89e0120d0e866c86a0792435972
maruf <maruf.hasan@tallykhata.com>
2025-09-18 11:59:26 +0600

    change in main file root level

--- BackTracking/permutations.py
+++ BackTracking/permutations.py
@@ -1,19 +1,21 @@
-nums = [1,2,3]
+nums = [4,5,6]
 
 #Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 
 
 
-def make_permutations(nums,i,sub_array,ans):
+def make_permutations(nums,i,sub_array,ans,used):
     if i == 0:
         ans.append([*sub_array])
-        return
-    for item in range(0,i,-1):
-        sub_array.append(nums[item])
-        make_permutations(nums,i-1,sub_array,ans)
-        sub_array.pop()
-
+    else:
+        for item in range(0,nums.__len__()):
+            if used[item] == False:
+                sub_array.append(nums[item])
+                used[item] = True
+                make_permutations(nums,i-1,sub_array,ans,used)
+                sub_array.pop()
+                used[item] = False
     return ans
 
 
-print(make_permutations(nums,nums.__len__(),[],[]))
+print(make_permutations(nums,nums.__len__(),[],[],[False]*nums.__len__()))
