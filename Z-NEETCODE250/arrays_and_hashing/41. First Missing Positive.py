a4870adcaea1ee1fa34dcc41fc62084a11aebe23
maruf <maruf.hasan@tallykhata.com>
2025-10-05 16:07:32 +0600

    change in main file root level

---
+++ Z-NEETCODE250/arrays_and_hashing/41. First Missing Positive.py
@@ -0,0 +1,18 @@
+
+
+def first_missing_positive(arr):
+    my_searching_range = len(arr)
+    i = 0
+
+    while i < my_searching_range:
+        item = arr[i]
+        if item <= my_searching_range:
+            existing_item = arr[item]
+            arr[item] = item
+
+
+
+
+
+
+print(first_missing_positive([6,7,8,1,2,3])) #1-6
